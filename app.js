da73bfc4e199bd204e51a0d0f3c6d822536b6b41
Tobi Maulana <tobimaulana.tm@gmail.com>
2022-12-04 05:36:25 +0700

    Update app.js for add a new jawaban route configuration

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var soalRouter = require('./routes/soal');
+var jawabanRouter = require('./routes/jawaban');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/soal', soalRouter);
+app.use('/jawaban', jawabanRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
