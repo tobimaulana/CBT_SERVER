d3d6b491ce2fed1424276df619445b266d586430
Tobi Maulana <tobimaulana.tm@gmail.com>
2022-12-04 05:33:37 +0700

    Create jawaban.js for routes
    The answer route is only used to add and display data (receives synchronization from the cbt project – later in part 2).

---
+++ routes/jawaban.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+
+//panggil Model soal & jawaban
+var Soal = require('../models/soal');
+var Jawaban = require('../models/jawaban');
+
+/* TAMPIL DATA */
+router.get('/', function(req, res, next) {
+       Jawaban.findAll().then( data => {
+              res.json({
+                     status:true,
+                     pesan:"Data BERHASIL Tampil",
+                     data:data
+              });
+       }).catch(err => {
+              res.json({
+                     status:false,
+              });
+       });
+});
+
+/* Menerima data dari project CBT untuk diupdate di TABEL JAWABAN*/
+router.post('/sync', async function(req, res, next) {
+       // Koreksi Jawaban
+       var jawaban = [];
+       for (var itemJawaban of req.body){
+              var soal = await Soal.findByPk(itemJawaban.soal_id);
+              if(soal.kunci == itemJawaban.jawaban) {
+                     itemJawaban.koreksi = 1;
+              } else {
+                     itemJawaban.koreksi = 0;
+              }
+              jawaban.push(itemJawaban);
+       }
+
+       Jawaban.bulkCreate(jawaban, {
+              updateOnDuplicate: ["jawaban","koreksi"]
+       }).then (data => {
+              res.json({
+                     status:true,
+                     pesan:"Berhasil Tersinkronisasi",
+                     data:data
+              });
+       })
+       .catch(err => {
+              res.json({
+                     status:false,
+                     pesan:"Gagal Tersinkronisasi : " + err.message,
+                     data:[]
+              });
+       });
+});
+
+module.exports = router;
